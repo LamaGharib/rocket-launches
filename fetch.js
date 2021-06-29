329b889f6c5cf3dfb5a346be82c9fd8b3ba88823
Lama Gharib <lamagharib3@gmail.com>
2021-06-29 17:53:23 +0200

    creating a fetch function

---
+++ fetch.js
@@ -0,0 +1,10 @@
+
+    export const fetchData = async(url) =>{
+    try{
+        const data = await fetch(url);
+        const allData = await data.json();
+        return allData
+    }catch{
+        (error) =>(error.message) ;
+    }
+};
