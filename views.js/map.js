e5991480152c0802ad7a3e0f0a9154a3a06f290f
Lama Gharib <lamagharib3@gmail.com>
2021-06-29 23:57:26 +0200

    fixing the map marker

--- views.js/map.js
+++ views.js/map.js
@@ -1,17 +1,14 @@
-export const addMap = (log, lan) => {
+export const addMap = (long, lan) => {
   mapboxgl.accessToken =
     "pk.eyJ1IjoibGFtYTMiLCJhIjoiY2txZ3MzM3J6MTI0bTJ1c2NkOXAwMXpnZiJ9.5kg3kLMUeLH24ikkpuJFlw";
 
   const map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/streets-v11",
-    center: [log, lan],
+    center: [long, lan],
     zoom: 14,
   });
 
-  const marker = new mapboxgl.Marker().setLngLat([
-    f.pad.longitude,
-    f.pad.latitude,
-  ]);
+  const marker = new mapboxgl.Marker().setLngLat([long, lan]);
   marker.addTo(map);
 };
